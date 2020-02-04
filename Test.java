63f41bb91367f5b2fd9aefe858b37e613c77395e
Your Name <you@example.com>
2020-02-04 08:13:46 +0800

    sayHello was added

--- Test.java
+++ Test.java
@@ -1,11 +1,9 @@
 class Test{
 		public static void main (String[]args){
 		System.out.println("Hello 1ISA");
-		
-		
-		
-		
-		
+		}
+		public void sayHello(){
+			System.out.print("Hello");
 		}
 		
 }
